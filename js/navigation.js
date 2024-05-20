f73db25a27fad176fc0f965b86b9d7c52db9bd46
Nahuel Morena <nahuel1morena1@gmail.com>
2024-05-20 10:28:23 -0300

    refactoring

--- js/navigation.js
+++ js/navigation.js
@@ -1,12 +1,8 @@
 // navigation.js
 document.addEventListener("DOMContentLoaded", function() {
     const navbarContainer = document.getElementById("navbarContainer");
-    let currentPage = window.location.pathname.split("/").pop(); // Obtener el nombre del archivo actual
-    console.log('Current Page:', currentPage);
-
-    currentPage = '/'+currentPage;
-
-    console.log('Current Page:', currentPage);
+    const currentPage = window.location.pathname.split("/").pop(); // Obtener el nombre del archivo actual
+    //currentPage = '/'+currentPage;
 
     // Fetch the navbar content
     fetch("navbar.html")
@@ -15,28 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
             // Set the fetched HTML as the content of the navbar container
             navbarContainer.innerHTML = html;
             
-            const menuItems = navbarContainer.querySelectorAll('.nav-menu-item');
-
-            menuItems.forEach(function(item) {
-                const link = item.querySelector('.nav-menu-link');
-                
-                let linkHref = link.getAttribute('href');
-                if (linkHref.endsWith('.html')){
-                    linkHref = linkHref.replace('.html','');
-                }
-
-                let pageWithoutExtension = currentPage;
-                if (currentPage.endsWith('.html')) {
-                    pageWithoutExtension = currentPage.replace('.html','');
-                }
-                pageWithoutExtension = '/'+pageWithoutExtension;
-
-                console.log('Comparing:', linkHref, pageWithoutExtension);
-
-                if (link.getAttribute('href') === currentPage) {
-                    link.classList.add('active');
-                }
-            });
+            highlightLink(navbarContainer, '/'+currentPage);
 
             // Acceder al botón hamburguesa después de cargar el contenido del navbar
             const menuBtn = navbarContainer.querySelector('.hamburger-menu');
@@ -47,3 +22,29 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         });
 });
+
+function highlightLink(navbarContainer, currentPage){
+    const menuItems = navbarContainer.querySelectorAll('.nav-menu-item');
+
+    menuItems.forEach(function(item) {
+        const link = item.querySelector('.nav-menu-link');
+        
+        let linkHref = link.getAttribute('href');
+        linkHref = replaceLink(linkHref);
+
+        let pageWithoutExtension = replaceLink(currentPage);
+
+        pageWithoutExtension = '/'+pageWithoutExtension;
+
+        if (link.getAttribute('href') === currentPage) {
+            link.classList.add('active');
+        }
+    });
+}
+
+function replaceLink(str){
+    if (str.endsWith('.html')) {
+        return str.replace('.html','');
+    }
+    return str;
+}
